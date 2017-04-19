1a2540c47c65f584b6973bec08c4f0a1c388f889
Adriana Cerrotti <adrioro@users.noreply.github.com>
2017-04-19 08:52:25 -0400

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,5 @@
+import my_extra_special_media
+
+toy_story = my_extra_special_media.Movie("Toy Story", "A story of a boy and his toys that come to life","https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=NBepTulmSMw")
+
+print(toy_story.movie_storyline)
